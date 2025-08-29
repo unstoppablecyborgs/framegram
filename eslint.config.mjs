3eaafa45fa16b06f261b24d6069df97de928a77f
Matvei <motya2222@gmail.com>
2025-08-29 13:46:22 +0300

    refactor: change eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,59 +1,108 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {FlatCompat} from '@eslint/eslintrc'
+
+// плагины
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import pluginImport from 'eslint-plugin-import'
+import pluginPrettier from 'eslint-plugin-prettier'
+import perfectionist from 'eslint-plugin-perfectionist'
+import tsParser from '@typescript-eslint/parser'
+import pluginTs from '@typescript-eslint/eslint-plugin'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 })
 
-export default [
-    ...compat.extends(
-        'next/core-web-vitals',
-        'next/typescript',
-        'plugin:prettier/recommended',
-        "plugin:storybook/recommended"
-    ),
-    {
-        plugins: {
-            prettier: require('eslint-plugin-prettier'),
-            'react-hooks-extra': require('eslint-plugin-react-hooks-extra'),
-        },
+const config = [
+  // базовые конфиги Next.js
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
-        rules: {
-            'prettier/prettier': [
-                'error',
-                {
-                    trailingComma: 'es5',
-                    semi: false,
-                    singleQuote: true,
-                    endOfLine: 'auto',
-                    printWidth: 100,
-                    arrowParens: 'avoid',
-                },
-            ],
-            'import/no-default-export': 'error',
-            'react-hooks-extra/no-direct-set-state-in-use-effect': 'error',
-        },
+  // твои кастомные правила
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 12,
+      sourceType: 'module',
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        }
     },
-    {
-        files: [
-            '*.stories.tsx',
-            '*.stories.ts',
-            'app/**/*.tsx',
-            'next.config.mjs',
-        ],
-        rules: {
-            'import/no-default-export': 'off',
-        },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      import: pluginImport,
+      prettier: pluginPrettier,
+      perfectionist,
+      '@typescript-eslint': pluginTs,
     },
-    {
-        files: ['src/**/*.{ts,tsx}'],
-        excludedFiles: ['**/*.stories.tsx'],
-        rules: {
-            'import/no-default-export': 'error',
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          paths: ['src'],
         },
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+      react: { version: 'detect' },
     },
+    rules: {
+      curly: ['error', 'all'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-nested-ternary': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'perfectionist/sort-imports': [
+          'error',
+          {
+              customGroups: {
+                  type: {
+                      react: ['react', 'react-*']
+                  }
+              },
+              groups: [
+                  'type',
+                  'react',
+                  'builtin',
+                  'external',
+                  'internal-type',
+                  'internal',
+                  'side-effect',
+                  'style'
+              ],
+              newlinesBetween: 'always', // тоже camelCase!
+              order: 'asc',
+              type: 'natural'
+          }
+      ],
+      'react/button-has-type': 'error',
+      'react/jsx-boolean-value': ['error'],
+      'react/jsx-fragments': ['error'],
+      'react/void-dom-elements-no-children': ['error'],
+    },
+  },
+
+  // игнорируем папки
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
 ]
+
+export default config
