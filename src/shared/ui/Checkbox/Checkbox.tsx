bf0f9f645420bb8277c23cd1e815356bf275e38b
Matvei <motya2222@gmail.com>
2025-09-02 15:07:02 +0300

    INC-3 feat: add checkbox component

--- src/shared/ui/Checkbox/Checkbox.tsx
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -1,12 +1,21 @@
+'use client'
+
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import clsx from 'clsx'
+import * as React from 'react'
+
+import { CheckIcon } from '@/shared/assets'
+
 import s from './Checkbox.module.scss'
 
-import { CheckIcon } from '../../assets'
-
-export const Checkbox = () => {
+function Checkbox({ className, ...props }: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
   return (
-    <div>
-      <input type="checkbox" />
-      <CheckIcon className={s.checkIcon} />
-    </div>
+    <CheckboxPrimitive.Root data-slot="checkbox" className={clsx(s.root, className)} {...props}>
+      <CheckboxPrimitive.Indicator data-slot="checkbox-indicator" className={s.indicator}>
+        <CheckIcon />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
   )
 }
+
+export { Checkbox }
