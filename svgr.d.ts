2ed970ec579239ff75a104a7be7a556add656222
Matvei <motya2222@gmail.com>
2025-09-03 12:15:37 +0300

    INC-3 refactor: change types for svgr

--- svgr.d.ts
+++ svgr.d.ts
@@ -1,5 +1,11 @@
 declare module '*.svg' {
-  import { SVGProps } from 'react';
-  const content: (props: SVGProps<SVGSVGElement>) => JSX.Element;
-  export default content;
+  import { FC, SVGProps } from 'react'
+  const content: FC<SVGProps<SVGElement>>
+  export default content
+}
+
+declare module '*.svg?url' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any
+  export default content
 }
