d7399854ea6f4ce43f0c86a2c20764b2b67ff87c
Matvei <motya2222@gmail.com>
2025-09-04 16:24:45 +0300

    INC-2 style: add basic styles for textarea

--- src/shared/ui/Textarea/Textarea.tsx
+++ src/shared/ui/Textarea/Textarea.tsx
@@ -1,8 +1,10 @@
 import clsx from 'clsx'
 import * as React from 'react'
 
+import styles from './Textarea.module.scss'
+
 function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
-  return <textarea data-slot="textarea" className={clsx(className)} {...props} />
+  return <textarea data-slot="textarea" className={clsx(styles.textarea, className)} {...props} />
 }
 
 export { Textarea }
