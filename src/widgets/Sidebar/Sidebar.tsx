621f3f68206922529f0392990ffdfe97399fa2aa
arseniiOsovets <arsenasavets@gmail.com>
2025-09-10 10:44:03 +0300

    refactor(sidebar): update SidebarLink component

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -1,27 +1,26 @@
 'use client'
 
-import * as React from 'react'
-import s from './Sidebar.module.scss'
-import Link from 'next/link'
-import SearchIcon from '@svg/outline/search-outline.svg'
-import TrendingIcon from '@svg/outline/trending-up-outline.svg'
-import HomeIcon from '@svg/outline/home-outline.svg'
+import BookMarkActiveIcon from '@svg/fill/bookmark.svg'
+import HomeActiveIcon from '@svg/fill/home.svg'
+import LogoutActiveIcon from '@svg/fill/log-out.svg'
+import MessageActiveIcon from '@svg/fill/message-circle.svg'
+import PersonActiveIcon from '@svg/fill/person.svg'
+import PlusActiveIcon from '@svg/fill/plus-square.svg'
+import SearchActiveIcon from '@svg/fill/search.svg'
+import TrendingActiveIcon from '@svg/fill/trending-up.svg'
 import BookMarkIcon from '@svg/outline/bookmark-outline.svg'
+import HomeIcon from '@svg/outline/home-outline.svg'
 import LogoutIcon from '@svg/outline/log-out-outline.svg'
 import MessageIcon from '@svg/outline/message-circle-outline.svg'
 import PersonIcon from '@svg/outline/person-outline.svg'
 import PlusIcon from '@svg/outline/plus-square-outline.svg'
+import SearchIcon from '@svg/outline/search-outline.svg'
+import TrendingIcon from '@svg/outline/trending-up-outline.svg'
 import clsx from 'clsx'
-
-import SearchActiveIcon from '@svg/fill/search.svg'
-import BookMarkActiveIcon from '@svg/fill/bookmark.svg'
-import LogoutActiveIcon from '@svg/fill/log-out.svg'
-import MessageActiveIcon from '@svg/fill/message-circle.svg'
-import HomeActiveIcon from '@svg/fill/home.svg'
-import PersonActiveIcon from '@svg/fill/person.svg'
-import PlusActiveIcon from '@svg/fill/plus-square.svg'
-import TrendingActiveIcon from '@svg/fill/trending-up.svg'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import * as React from 'react'
+import s from './Sidebar.module.scss'
 import { SidebarLink } from './SidebarLink/SidebarLink'
 
 export const Sidebar = () => {
@@ -36,35 +35,34 @@ export const Sidebar = () => {
       label: 'Feed',
       iconDefault: HomeIcon,
       iconActive: HomeActiveIcon,
-      
     },
     {
       id: 2,
       href: '/create',
       label: 'Create',
       iconDefault: PlusIcon,
-      iconActive: PlusActiveIcon
+      iconActive: PlusActiveIcon,
     },
     {
       id: 3,
       href: '/profile',
       label: 'My Profile',
       iconDefault: PersonIcon,
-      iconActive: PersonActiveIcon
+      iconActive: PersonActiveIcon,
     },
     {
       id: 4,
       href: '/messenger',
       label: 'Messenger',
       iconDefault: MessageIcon,
-      iconActive: MessageActiveIcon
+      iconActive: MessageActiveIcon,
     },
     {
       id: 5,
       href: '/search',
       label: 'Search',
       iconDefault: SearchIcon,
-      iconActive: SearchActiveIcon
+      iconActive: SearchActiveIcon,
     },
   ]
 
@@ -74,14 +72,14 @@ export const Sidebar = () => {
       href: '/statistics',
       label: 'Statistics',
       iconDefault: TrendingIcon,
-      iconActive: TrendingActiveIcon
+      iconActive: TrendingActiveIcon,
     },
     {
       id: 2,
       href: '/favorites',
       label: 'Favorites',
       iconDefault: BookMarkIcon,
-      iconActive: BookMarkActiveIcon
+      iconActive: BookMarkActiveIcon,
     },
   ]
 
@@ -90,7 +88,13 @@ export const Sidebar = () => {
       <div className={clsx(s.block, s.block1)}>
         {topLinks.map(el => {
           return (
-            <SidebarLink key={el.id} label={el.label} href={el.href} iconActive={el.iconActive} iconDefault={el.iconDefault}/>
+            <SidebarLink
+              key={el.id}
+              label={el.label}
+              href={el.href}
+              iconActive={el.iconActive}
+              iconDefault={el.iconDefault}
+            />
           )
         })}
       </div>
@@ -98,16 +102,20 @@ export const Sidebar = () => {
       <div className={clsx(s.block, s.block2)}>
         {middleLinks.map(el => {
           return (
-            <SidebarLink key={el.id} label={el.label} href={el.href} iconActive={el.iconActive} iconDefault={el.iconDefault}/> 
-
-            
+            <SidebarLink
+              key={el.id}
+              label={el.label}
+              href={el.href}
+              iconActive={el.iconActive}
+              iconDefault={el.iconDefault}
+            />
           )
         })}
       </div>
 
       <div className={s.block}>
         {/* снизу вместо isDisabled прописать типа el.href === pathname */}
-        <Link href="/logout" className={clsx(s.link, { [s.disabled]: isDisabled })}>
+        <Link href={{ pathname: '/logout' }} className={clsx(s.link, { [s.disabled]: isDisabled })}>
           {isLogoutPage ? (
             <LogoutActiveIcon width={24} height={24} />
           ) : (
@@ -119,5 +127,3 @@ export const Sidebar = () => {
     </nav>
   )
 }
-
-
--- src/widgets/Sidebar/SidebarLink/SidebarLink.tsx
+++ src/widgets/Sidebar/SidebarLink/SidebarLink.tsx
@@ -1,14 +1,14 @@
-import clsx from "clsx"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import clsx from 'clsx'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { FC, SVGProps } from 'react'
 import s from '../Sidebar.module.scss'
 
 type Props = {
-  
   label: string
   href: string
-  iconActive: any
-  iconDefault: any
+  iconActive: FC<SVGProps<SVGElement>>
+  iconDefault: FC<SVGProps<SVGElement>>
 }
 
 export const SidebarLink = ({ href, label, iconActive, iconDefault }: Props) => {
@@ -18,8 +18,7 @@ export const SidebarLink = ({ href, label, iconActive, iconDefault }: Props) => 
   const Icon = isActive ? iconActive : iconDefault
 
   return (
-    
-    <Link href={href} className={clsx(s.link, { [s.disabled]: isDisabled })}>
+    <Link href={{ pathname: href }} className={clsx(s.link, { [s.disabled]: isDisabled })}>
       <Icon width={24} height={24} />
       <h4 className={isActive ? s.active : s.link}> {label}</h4>
     </Link>
