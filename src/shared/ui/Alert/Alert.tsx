4d8d03cfaac30c387a7e1b4bcefa1e83a6849d0d
Matvei <motya2222@gmail.com>
2025-09-16 18:31:48 +0300

    INC-6 refactor: change Alert component

--- src/shared/ui/Alert/Alert.tsx
+++ src/shared/ui/Alert/Alert.tsx
@@ -1,65 +1,39 @@
 'use client'
 
 import clsx from 'clsx'
-import { useEffect, useState } from 'react'
-
-import { Typography } from '@/shared/ui'
-
+import { CloseIcon } from '@/shared/assets'
 import styles from './Alert.module.scss'
 
-export type AlertVariants = 'error' | 'succeeded'
+type AlertVariant = 'error' | 'success'
 
-type Props = {
-  variant: AlertVariants
-  autoHideDuration?: number
-  message: string
+type Props = React.ComponentProps<'div'> & {
+  variant?: AlertVariant
+  onClose: () => void
 }
 
-export const Alert = (props: Props) => {
-  const { message, variant, autoHideDuration = 6000 } = props
-
-  const [isVisible, setVisible] = useState(true)
-
-  const onClose = () => {
-    setVisible(false)
-  }
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setVisible(false)
-    }, autoHideDuration)
-    return () => {
-      clearTimeout(timeoutId)
-    }
-  }, [autoHideDuration])
-
+function Alert({ children, className, variant = 'error', onClose, ...restProps }: Props) {
   return (
-    isVisible && (
-      <div className={clsx(styles.container)}>
-        <div
-          className={clsx(
-            styles.alert,
-            variant === 'error' && styles['alertError'],
-            variant === 'succeeded' && styles['alertSucceeded']
-          )}
-        >
-          <Typography className={styles.alertDescription}>{message}</Typography>
-          <svg
-            className={styles.close}
-            onClick={onClose}
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
-              fill="white"
-            />
-          </svg>
-        </div>
+    <div
+      data-slot="alert"
+      role="alert"
+      className={clsx(styles.root, styles[variant], className)}
+      {...restProps}
+    >
+      <div>
+        {variant === 'error' && <span className={styles.errorTitle}>Error!</span>}
+        {children}
       </div>
-    )
+      <button
+        type="button"
+        aria-label="Close"
+        title="Close"
+        className={styles.closeButton}
+        onClick={onClose}
+      >
+        <CloseIcon aria-hidden={true} />
+      </button>
+    </div>
   )
 }
+
+export { Alert }
