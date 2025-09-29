72e69964e5b0a5fc99e58857b6d6432455eb707c
Matvei <motya2222@gmail.com>
2025-09-29 14:08:21 +0300

    feat: add passwords compare for confirmPassword

--- src/features/auth/signup/model/signupSchema.ts
+++ src/features/auth/signup/model/signupSchema.ts
@@ -3,12 +3,17 @@ import { emailSchema, passwordSchema } from '@/shared/auth'
 import { termsSchema } from './termsSchema'
 import { usernameSchema } from './usernameSchema'
 
-export const signupSchema = z.object({
-  username: usernameSchema,
-  email: emailSchema,
-  password: passwordSchema,
-  confirmPassword: z.string(),
-  acceptTerms: termsSchema,
-})
+export const signupSchema = z
+  .object({
+    username: usernameSchema,
+    email: emailSchema,
+    password: passwordSchema,
+    confirmPassword: z.string(),
+    acceptTerms: termsSchema,
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  })
 
 export type SignupFormFields = z.infer<typeof signupSchema>
